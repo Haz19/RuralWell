364980bbc8448edc8d4312504ea19fded17a83b7
Haz19 <haz_tars08@hotmail.com>
2026-04-12 22:22:53 -0600

    Creación del proyecto

---
+++ src/main/kotlin/ss/serviciosocial/ServicioSocialApplication.kt
@@ -0,0 +1,11 @@
+package ss.serviciosocial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServicioSocialApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServicioSocialApplication>(*args)
+}
